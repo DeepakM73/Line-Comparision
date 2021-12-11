522035a01a39457d09332b401d355638ead3e963
DeepakM73 <deepak.muddasani73@gmail.com>
2021-12-11 07:27:25 +0530

    Added: Comparing Lines

--- src/com/bridgelabz/linecomp/LineComparision.java
+++ src/com/bridgelabz/linecomp/LineComparision.java
@@ -35,6 +35,11 @@ public class LineComparision {
             System.out.println("Two Lines are Equal");
         } else {
             System.out.println("Two Lines are Not Equal");
+            if (lengthOfLine1 < lengthOfLine2) {
+                System.out.println("Line1 is less than Line2");
+            } else {
+                System.out.println("Line2 is less than Line1");
+            }
         }
     }
 }
