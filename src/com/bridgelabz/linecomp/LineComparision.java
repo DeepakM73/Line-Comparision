b275557bc58c25262e8628374a7ca1125be873a7
DeepakM73 <deepak.muddasani73@gmail.com>
2021-12-11 07:24:40 +0530

    Added: Means to calculate Equality of lines

--- src/com/bridgelabz/linecomp/LineComparision.java
+++ src/com/bridgelabz/linecomp/LineComparision.java
@@ -17,5 +17,24 @@ public class LineComparision {
 
         double lengthOfLine1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         System.out.println("Length of Line 1 = " + lengthOfLine1);
+
+        System.out.println("Enter the values of next co-ordinates ");
+        System.out.print("Enter the value of (x3,y3)= ");
+        int x3 = SC.nextInt();
+        int y3 = SC.nextInt();
+        System.out.print("Enter the value of (x4,y4)= ");
+        int x4 = SC.nextInt();
+        int y4 = SC.nextInt();
+
+        double lengthOfLine2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+
+        System.out.println("Length of Line 1 = " + lengthOfLine1);
+        System.out.println("Length of Line2= " + lengthOfLine2);
+
+        if (lengthOfLine1 == lengthOfLine2) {
+            System.out.println("Two Lines are Equal");
+        } else {
+            System.out.println("Two Lines are Not Equal");
+        }
     }
 }
